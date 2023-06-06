2ce0f50f660b2c26b53bb786d5e6c6acc4480307
Devops Easy Learning <96950933+devopseasylearning@users.noreply.github.com>
2023-06-05 19:00:44 -0500

    Update key.sh

--- key.sh
+++ key.sh
@@ -1,16 +1,23 @@
 #! /bin/bash
 
 ### checking if you have a key
-read -p "Enter your prefer_name: " NAME
+# read -p "Enter your prefer_name: " NAME
 if 
-   [[ -z ${NAME} ]]
+   [[ $# -ne 1 ]]
+then
+echo "this script required only one argument which is your prefer name to run"
+ exit 1
+fi 
+
+if 
+   [[ -z $1 ]]
 then
 echo "your did not enter your prefer name"
  exit 1
 fi 
 
 
-SESSION=`echo ${NAME} |  awk '{print substr($0,1,2)}'`
+SESSION=`echo $1 |  awk '{print substr($0,1,2)}'`
 
 if 
 [[ ${SESSION} == s2 ]] || [[ ${SESSION} == s3 ]] || [[ ${SESSION} == s4 ]] || [[ ${SESSION} == s5 ]] || [[ ${SESSION} == s6 ]] || [[ ${SESSION} == s7 ]]
@@ -29,10 +36,10 @@ if
   [[ ${KEY} == id_rsa.pub  ]]
 then 
 echo "you are good to go "
-rm -rf ~/Downloads/${NAME}.pem || true
-cp -r ~/.ssh/id_rsa.pub  ~/Downloads/${NAME}.pem
+rm -rf ~/Downloads/$1.pem || true
+cp -r ~/.ssh/id_rsa.pub  ~/Downloads/$1.pem
 
-ls -l | grep -io ~/Downloads/${NAME}.pem
+ls -l | grep -io ~/Downloads/$1.pem
 echo "KEY COPY "
 elif 
   [[ ${KEY} != id_rsa.pub  ]] 
@@ -52,11 +59,16 @@ git clone https://devopseasylearning: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@gi
 git config --global user.name "devopseasylearning"
 git config --global user.email info@devopseasylearning.com
 cd Server-login-key/keys
-cat  ~/.ssh/id_rsa.pub > ./${NAME}.pem
+cat  ~/.ssh/id_rsa.pub > ./$1.pem
 git add -A 
-git commit -m "adding ${NAME}.pem"
+git commit -m "adding $1.pem"
 git push https://devopseasylearning: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@github.com/devopseasylearning/Server-login-key.git 
 
 cd - && rm -rf Server-login-key
 echo "INSTRUCTIONS"
 echo "PLEASE NOTIFY ERIC VIA WHATSAPP at 832-342-0700"
+
+clear 
+sleep 3
+echo "##########################"
+echo "yah!!!!!!!!!!!!!!!!!!! $1 YOU MADE IT"
